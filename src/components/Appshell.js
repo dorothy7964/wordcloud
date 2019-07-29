da8495105184370d9fbba56cfe09164f73cde1e7
sample <dorothy_007@naver.com>
2019-07-30 03:37:11 +0900

    Add Router

--- src/components/Appshell.js
+++ src/components/Appshell.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Drawer from '@material-ui/core/Drawer';
@@ -35,8 +37,25 @@ class AppShell extends React.Component {
                     </IconButton>
                 </AppBar>
                 <Drawer open={this.state.toggle}>
-                    <MenuItem onClick={this.handleDrawerToggle}>Home</MenuItem>
+                    <MenuItem onClick={this.handleDrawerToggle}>
+                        <Link component={RouterLink} to="/">
+                            Home
+                        </Link>
+                    </MenuItem>
+                    <MenuItem onClick={this.handleDrawerToggle}>
+                        <Link component={RouterLink} to="/texts">
+                            Texts
+                        </Link>
+                    </MenuItem>
+                    <MenuItem onClick={this.handleDrawerToggle}>
+                        <Link component={RouterLink} to="/words">
+                            Words
+                        </Link>
+                    </MenuItem>
                 </Drawer>
+                <div id="content" style={{margin: 'auto', marginTop: '20px'}}>
+                    {React.cloneElement(this.props.children)}
+                </div>
             </div>
         );
     }
